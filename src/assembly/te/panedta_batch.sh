853f99ff0d75fde6de11984db104064dc41d34ed
AGanesan12 <112353366+AGanesan12@users.noreply.github.com>
2025-09-21 18:22:10 -0400

    Delete src/assembly/te directory

--- src/assembly/te/panedta_batch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# panEDTA across multiple genomes (uses local symlinks if paths are messy)
-# Usage: ./22_panedta_batch.sh <genome_list.txt> [threads=128] [cds.fa (optional)]
-list="$1"; thr="${2:-128}"; cds="${3:-}"
-if [[ -n "$cds" ]]; then
-  panEDTA/panEDTA.sh -g "$list" -c "$cds" -t "$thr"
-else
-  panEDTA/panEDTA.sh -g "$list" -t "$thr"
-fi
